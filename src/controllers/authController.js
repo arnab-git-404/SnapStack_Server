9943d5afd974709655bb96291a799da6e6513208
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-10-23 05:55:36 +0530

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -81,7 +81,7 @@ const login = async (req, res) => {
   const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
-    return res.status(401).json({
+    return res.status(404).json({
       success: false,
       message: "Invalid Email",
     });
