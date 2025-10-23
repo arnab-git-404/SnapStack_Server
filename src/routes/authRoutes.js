8eb62b34fd776ffe537d91b615703c0a2aa5b722
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-10-23 07:41:09 +0530

    reset password Routes defined

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const validateRequest = require('../middleware/validateRequest');
-const { register, login, logout, verifyToken, refreshAccessToken, authValidation } = require('../controllers/authController');
+const validateRequest = require("../middleware/validateRequest");
+const {
+  register,
+  login,
+  logout,
+  verifyToken,
+  refreshAccessToken,
+  forgotPassword,
+  resetPassword,
+  authValidation,
+} = require("../controllers/authController");
 
-router.post('/register', authValidation.register, validateRequest, register);
-router.post('/login', authValidation.login, validateRequest, login);
-router.post('/logout', logout); // If using cookies
-router.post('/validate', refreshAccessToken); // If using refresh tokens
+router.post("/register", authValidation.register, validateRequest, register);
+router.post("/login", authValidation.login, validateRequest, login);
+router.post("/logout", logout); // If using cookies
+router.post("/validate", refreshAccessToken); // If using refresh tokens
+router.post(
+  "/forgot-password",
+  authValidation.forgotPassword,
+  validateRequest,
+  forgotPassword
+);
+router.post(
+  "/reset-password/:token",
+  authValidation.resetPassword,
+  validateRequest,
+  resetPassword
+);
 
 module.exports = router;
-
-
-
